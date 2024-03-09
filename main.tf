493537d95d4b22bcc257c67542a629a8277d7979
Sudhamshetty7 <154966370+Sudhamshetty7@users.noreply.github.com>
2024-03-09 14:33:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 resource "aws_instance" {
   ami           = "ami-07d9b9ddc6cd8dd30"
   instance_type = "t2.medium"
-  
+  security_groups = [aws_security_group.example_sg.name]
   user_data = file("docker.sh")
-  
   tags = {
     Name = "Assigment-docker1"
+  }
+}
+resource "aws_security_group" "example_sg" {
+  name        = "sg"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "sg"
   }
 }
